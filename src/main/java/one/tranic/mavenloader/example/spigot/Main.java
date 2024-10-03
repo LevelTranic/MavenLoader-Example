b9d6860265bd63a863b49309aa11c798e167c053
404Setup <153366651+404Setup@users.noreply.github.com>
2024-10-03 20:13:41 +0800

    Remove old API references

--- src/main/java/one/tranic/mavenloader/example/spigot/Main.java
+++ src/main/java/one/tranic/mavenloader/example/spigot/Main.java
@@ -5,14 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        // If you choose to store maven.yml in Resources, you do not need to do it again here.
-        /*try {
-            MavenLibraryResolver resolver = new MavenLibraryResolver();
-            resolver.addDependency("org.jooq:jooq:3.17.7");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }*/
-
         try {
             new org.jooq.util.xml.jaxb.Catalog();
         } catch (Exception e) {
--- src/main/java/one/tranic/mavenloader/example/velocity/Main.java
+++ src/main/java/one/tranic/mavenloader/example/velocity/Main.java
@@ -5,7 +5,6 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
-//import one.tranic.mavenloader.api.MavenLibraryResolver;
 import org.slf4j.Logger;
 
 @Plugin(id = "maven-loader-example", name = "MavenLoaderExample", version = BuildConstants.VERSION, url = "https://tranic.one", authors = {"404"}, dependencies = {@Dependency(id = "maven-loader")})
@@ -16,14 +15,6 @@ public class Main {
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
-        // If you choose to store maven.yml in Resources, you do not need to do it again here.
-        /*try {
-            MavenLibraryResolver resolver = new MavenLibraryResolver();
-            resolver.addDependency("org.jooq:jooq:3.17.7");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }*/
-
         try {
             new org.jooq.util.xml.jaxb.Catalog();
         } catch (Exception e) {
